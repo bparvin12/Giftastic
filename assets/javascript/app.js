a6dc5d8184b426c4b4f7e5614b88529c12f49da1
bparvin12 <bparvin12@gmail.com>
2018-11-27 17:26:52 -0800

    start of psuedocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// create a variable with a few sports teams 
+var sports = []
+
+//function for displaying sports data
+function renderButtons() {
+
+    //delete teams prior to adding new teams 
+    //we do this so we dont have repeat buttons 
+    $(".buttons-view").empty(); 
+
+    //create a loop to loop through array of teams 
+
+    //create button for each team in array 
+}
+
