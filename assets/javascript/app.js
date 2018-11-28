7afe1bb75be470e0430084cd38cc11afbc0ef467
bparvin12 <bparvin12@gmail.com>
2018-11-27 18:30:49 -0800

    getting an error on console saying ajax is not a function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,29 @@
 var sports = ["LA Lakers", "LA Rams", "Pittsburgh Steelers"];
 
 //need a function to add gif when button is clicked. using $(this).attr("data-name")
+function displayGif () {
+    //creating variable that contains button name (data-name)
+    var teamGif = $(this).attr("data-name");
+    teamGif = teamGif.trim().replace(/ /g, "+");
+    //testing if the right name appears
+    //commented this out below
+    // alert(teamGif);
 
+    //using ajax now to obtain info from giphy website
+    //remeber we can use .gitIgnore for API key
+    var APIKey = "&api_key=1wM065kHhHtOptZ7714iuCSFEXbhCE4P";
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + teamGif + APIKey;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+
+        console.log(response);
+    });
+
+};
 
 
 //function for displaying sports data
@@ -10,7 +32,7 @@ function renderButtons() {
 
     //delete teams prior to adding new teams 
     //we do this so we dont have repeat buttons 
-    $(".button-view").empty(); 
+    $("#button-view").empty(); 
 
     //create a loop to loop through array of teams 
     for (var i = 0; i < sports.length; i++) {
@@ -23,6 +45,24 @@ function renderButtons() {
     button.text(sports[i]);
     $("#button-view").append(button);
     }
-}
+};
 
 //create a function that adds the user input into array 
+$("#add-team").on('click', function(event){
+    event.preventDefault();
+
+    //grabbing the input from text box
+    //trim eliminates spaces
+    var team = $("#sport-input").val().trim();
+
+    //add the text into our previous sports array
+    sports.push(team);
+
+    renderButtons();
+});
+
+
+
+//invoking the functions
+$(document).on('click', '.team', displayGif);
+renderButtons();
